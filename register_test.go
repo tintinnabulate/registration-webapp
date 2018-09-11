4fddf47e3abc63258456065cb5636eb5a24f6697
Tintinnabulate <tintinn@abulate>
2018-09-11 19:36:13 +0100

    Write failing test

--- register_test.go
+++ register_test.go
@@ -4,16 +4,19 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
+	"strconv"
+	"strings"
 	"testing"
 
 	c "github.com/smartystreets/goconvey/convey"
 
 	"golang.org/x/net/context"
+	"google.golang.org/appengine"
 	"google.golang.org/appengine/aetest"
-	//"google.golang.org/appengine"
 )
 
-func CreateContextHandlerToHttpHandler(ctx context.Context) ContextHandlerToHandlerHOF {
+func CreateContextHandlerToHTTPHandler(ctx context.Context) ContextHandlerToHandlerHOF {
 	return func(f ContextHandlerFunc) HandlerFunc {
 		return func(w http.ResponseWriter, r *http.Request) {
 			f(ctx, w, r)
@@ -21,57 +24,46 @@ func CreateContextHandlerToHttpHandler(ctx context.Context) ContextHandlerToHand
 	}
 }
 
-func TestPostRegistrationHandler(t *testing.T) {
+func getContext() (context.Context, aetest.Instance) {
+	inst, _ := aetest.NewInstance(
+		&aetest.Options{
+			StronglyConsistentDatastore: true,
+			// SuppressDevAppServerLog:     true,
+		})
+	req, err := inst.NewRequest("GET", "/", nil)
+	if err != nil {
+		inst.Close()
+	}
+	ctx := appengine.NewContext(req)
+	return ctx, inst
+}
+
+// TestCreateSignupEndpoint tests that we can create a signup
+func TestCreateSignupEndpoint(t *testing.T) {
 	ConfigInit()
 
-	ctx, _, _ := aetest.NewContext()
+	ctx, inst := getContext()
+	defer inst.Close()
 
-	c.Convey("When user tries to register with an unverified email address", t, func() {
-		r := CreateHandler(CreateContextHandlerToHttpHandler(ctx))
+	c.Convey("When you submit a blank email address", t, func() {
+		r := CreateHandler(CreateContextHandlerToHTTPHandler(ctx))
 		record := httptest.NewRecorder()
 
-		req, err := http.NewRequest("POST", "/register", nil)
+		formData := url.Values{}
+		formData.Set("Email_Address", "")
+
+		req, err := http.NewRequest("POST", "/signup", strings.NewReader(formData.Encode())) // URL-encoded payload
+		//req.Header.Add("Authorization", "auth_token=\"XXXXXXX\"")
+		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+		req.Header.Add("Content-Length", strconv.Itoa(len(formData.Encode())))
+
 		c.So(err, c.ShouldBeNil)
 
-		c.Convey("It should return a 200 response, but suggest /signup", func() {
+		c.Convey("The next page body should contain \"Please check your email...\"", func() {
 			r.ServeHTTP(record, req)
 			c.So(record.Code, c.ShouldEqual, 200)
-			c.So(fmt.Sprint(record.Body), c.ShouldEqual, "Please sign up first /signup")
+			c.So(fmt.Sprint(record.Body), c.ShouldContainSubstring, `Please check your email inbox, and click the link we've sent you
+			`)
 		})
 	})
 }
-
-/*
-func TestGetRegistrationHandler(t *testing.T) {
-	c.Convey("If you do not sign up", t, func() {
-
-		c.Convey("...then register", func() {
-
-			c.Convey("it should return 200 response and suggest /signup", nil)
-
-		})
-
-	})
-
-	c.Convey("If you sign up first", t, func() {
-
-		c.Convey("...but forget to verify", func() {
-
-			c.Convey("it should return 200 response and suggest /signup", nil)
-
-		})
-
-		c.Convey("...and verify", func() {
-
-			c.Convey("...then register", func() {
-
-				c.Convey("it should return 200 response and succeed", nil)
-
-			})
-
-		})
-
-	})
-
-}
-*/
