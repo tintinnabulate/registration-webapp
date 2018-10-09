9a414e966fbac16f2ab2bd4c866a9c57f3505d17
Tintinnabulate <tintinn@abulate>
2018-10-10 00:07:13 +0100

    Remove unused functions

--- db_operations.go
+++ db_operations.go
@@ -18,31 +18,6 @@ import (
 	"golang.org/x/net/context"
 )
 
-// stashRegistrationForm : adds a signup with the given verification code to the datastore,
-// returning the key of the newly created entity.
-func stashRegistrationForm(ctx context.Context, regform *registrationForm) (*datastore.Key, error) {
-	regform.Creation_Date = time.Now()
-	key := datastore.NewKey(ctx, "RegistrationForm", regform.Email_Address, 0, nil)
-	k, err := datastore.Put(ctx, key, regform)
-	if err != nil {
-		return nil, fmt.Errorf("could not store registration form in registration form table: %v", err)
-	}
-	return k, nil
-}
-
-// getRegistrationForm : gets the registration form matching the given email address.
-// This should only be called during testing.
-func getRegistrationForm(ctx context.Context, email string) (registrationForm, error) {
-	key := datastore.NewKey(ctx, "RegistrationForm", email, 0, nil)
-	var regform registrationForm
-	err := datastore.Get(ctx, key, &regform)
-	if err != nil {
-		return registrationForm{},
-			fmt.Errorf("could not get registration form entry: %v", err)
-	}
-	return regform, nil
-}
-
 // addUser : adds user to User table
 func addUser(ctx context.Context, u *user) (*datastore.Key, error) {
 	key := datastore.NewKey(ctx, "User", u.Email_Address, 0, nil)
